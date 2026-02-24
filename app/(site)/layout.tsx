51964806269092c5c4e58c440e67e571c337797d
Nora <noravelde@gmail.com>
2026-02-24 14:56:14 +0100

    force dynamic test, layout cache issue

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -53,6 +53,8 @@ export const metadata: Metadata = {
   description: "Official Website of Ted Duchamp",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: Readonly<{

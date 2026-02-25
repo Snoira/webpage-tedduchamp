a56b9a3313ec328ff53cebca33efca523c2354e0
Nora <noravelde@gmail.com>
2026-02-25 14:57:41 +0100

    req.url->siteUrl

--- app/api/draft-mode/enable/route.ts
+++ app/api/draft-mode/enable/route.ts
@@ -1,12 +1,11 @@
 import { draftMode } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { siteUrl } from "@/env";
 
 export async function GET(req: NextRequest) {
-  console.log("Enabling draft mode");
-
   (await draftMode()).enable();
 
   const redirectUrl = req.nextUrl.searchParams.get("redirect") || "/";
 
-  return NextResponse.redirect(new URL(redirectUrl, req.url));
+  return NextResponse.redirect(new URL(redirectUrl, siteUrl));
 }
